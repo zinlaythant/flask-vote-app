dac846934d73daa350bad982d80037deaa571f19
sjbylo <Stephen.Bylo@yahoo.com.sg>
2019-01-30 17:08:02 +0800

    Changed port from 5000 to 8080

--- app.py
+++ app.py
@@ -124,5 +124,5 @@ if __name__ == '__main__':
           print "Cannot load seed data from file"
           poll = Poll("", "")
     
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
 
